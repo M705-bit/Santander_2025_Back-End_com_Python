ecb0080a9744428218d50b4e9f4ef255e008bb93
Marina Hedlund Jardim <84291180+M705-bit@users.noreply.github.com>
2025-07-21 14:46:43 -0300

    Update arquivo.py

--- arquivo.py
+++ arquivo.py
@@ -1,81 +1,58 @@
-class novoPaciente():
-     def __init__(self, nome, idade, estado, prioridade = 0):
-        self.nome = nome
-        self.idade = idade
-        self.estado = estado
-        self.prioridade = 0 
-
-def seta_prioridade(lista):
-
-    for i in range(len(lista)):
-
-        if lista[i].estado == 'urgente' and paciente.idade >= 60: 
-            lista[i].prioridade = 1
-        elif lista[i].estado == 'urgente' and paciente.idade < 60: 
-            lista[i].prioridade = 2
-        elif lista[i].estado == 'normal' and paciente.idade > 60: 
-            lista[i].prioridade = 3
-        else:
-            lista[i].prioridade = 4
-def ordena(lista):
-    lista1 = []
-    for i in range(len(lista)):
-        if lista[i].prioridade == 1:
-            lista1.insert(0, paciente)
-        elif lista[i].prioridade == 2:
-             for i in range(len(lista)):
-            if lista[i].prioridade != "urgente":
-                lista.insert(i, paciente)
-                inserido = True
-                break
-        if not inserido:
-            lista.append(paciente)
-            lista1.insert(i, paciente)
-        elif lista[i].prioridade == 3:
-
-'''
-def add_paciente(lista, nome, idade, estado):
-
-    paciente = novoPaciente(nome, idade, estado)
-
-    if paciente.estado == 'urgente' and paciente.idade >= 60:
-        lista.insert(0, paciente)
-    elif paciente.estado == 'urgente' and paciente.idade < 60:
-        for i in range(len(lista)):
-             if lista[i].estado == 
-                lista.insert(i, paciente)
-                inserido = True
-                break
-             
-    elif paciente.idade > 60:
-        inserido = False
-        for i in range(len(lista)):
-            if lista[i].estado != "urgente":
-                lista.insert(i, paciente)
-                inserido = True
-                break
-        if not inserido:
-            lista.append(paciente)
-
-    else:
-        lista.append(paciente)
-'''
-
+# Entrada do número de pacientes
 n = int(input().strip())
-lista = []
+
+# Lista para armazenar pacientes
+pacientes = []
+
+# Loop para entrada de dados
 for _ in range(n):
-    nome, idade, estado = input().strip().split(", ")
-    paciente = novoPaciente(nome, int (idade), estado)
-    lista.append(paciente)
+    nome, idade, status = input().strip().split(", ")
+    idade = int(idade)
+    pacientes.append((nome, idade, status))
 
-seta_prioridade(lista)
+# TODO: Ordene por prioridade: urgente > idosos > demais:
+
+
+def ordena(fila_de_atendimento):
     
+    tam = len(fila_de_atendimento)
+    fila_urgente = []
+    fila_n_urgente = []
+    for i in range(tam):
+            if fila_de_atendimento[i][2] == "urgente":
+                     fila_urgente.append(fila_de_atendimento[i])   
+            else:
+                 fila_n_urgente.append(fila_de_atendimento[i]) 
 
+    def bubble_sort(array):
+        tam=len(array)
+        for i in range(tam):
+            for j in range(0, tam-1):
+            		if array[j][1] < array[j+1][1]:
+                         temp = array[j]
+                         array[j] = array[j+1]
+                         array[j+1] = temp 
+        return array
+    
+    fila_urgente = bubble_sort(fila_urgente)
+    fila_n_urgente = bubble_sort(fila_n_urgente)
+
+    array = []
+
+    for i in range(len(fila_urgente)):
+         array.append(fila_urgente[i])
+    for i in range(len(fila_n_urgente)):
+         array.append(fila_n_urgente[i])
+
+    return array 
+    
+array = ordena(pacientes)
+
+# TODO: Exiba a ordem de atendimento com título e vírgulas:
 print("Ordem de atendimento: ", end="")
 
 for i in range(n):
-    if i > 0:
-        print(f", {lista[i].nome}", end="")
-    else:
-         print(lista[i].nome, end="")
-
+        if i > 0:
+            print(f", {array[i][0]}", end="")
+        else:
+            print(f"{array[i][0]}", end="")
